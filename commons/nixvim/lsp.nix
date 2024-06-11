0b0dc4448efcd245663a6949f7410d964c9d6e5d
Florentin Labelle <101594032+florentinl@users.noreply.github.com>
2024-06-11 23:14:56 +0200

    lsp stuff

--- commons/nixvim/lsp.nix
+++ commons/nixvim/lsp.nix
@@ -2,13 +2,14 @@
   programs.nixvim.plugins.lsp = {
     enable = true;
     servers = {
-      nil_ls = {
+      nil-ls = {
         enable = true;
         extraOptions.settings = {
           nil.formatting.command = ["nix" "fmt" "--" "-"];
         };
       };
       pyright.enable = true;
+      ruff.enable = true;
     };
     keymaps.lspBuf = {
       "gd" = "definition";
