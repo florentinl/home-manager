814294450ad2261c169877d695e829763e212f03
Florentin Labelle <101594032+florentinl@users.noreply.github.com>
2024-08-19 11:03:42 +0200

    remove fold autocmd

--- commons/nixvim/autocmd.nix
+++ commons/nixvim/autocmd.nix
@@ -1,14 +1,6 @@
 {
   programs.nixvim.autoCmd = [
     {
-      desc = "Unfold everything when opening a file";
-      event = [
-        "BufEnter"
-        "BufWinEnter"
-      ];
-      command = "normal! zR";
-    }
-    {
       desc = "Format with LSP on save when possible";
       event = [ "BufWritePre" ];
       command = "lua vim.lsp.buf.format()";
